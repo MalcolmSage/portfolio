7654449e2ed7731ef9d303937c5a7d64d6c3ea3f
ThatLivelyLlama <thatlivelyllama@gmail.com>
2021-04-29 12:54:37 -0600

    Edited header to allow for elevated scrolling, as well as placed down a grid template in app.

--- src/components/header.js
+++ src/components/header.js
@@ -6,8 +6,10 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import { makeStyles } from '@material-ui/core/styles';
 import { NavLink } from 'react-router-dom';
 import Link from '@material-ui/core/Link';
-import Button from '@material-ui/core/Button';
 import SimpleMenu from './menu'
+import PropTypes from 'prop-types';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+
 
 const useStyles = makeStyles((theme) => ({
 	appBar: {
@@ -21,36 +23,63 @@ const useStyles = makeStyles((theme) => ({
 	},
 }));
 
-function Header() {
+function ElevationScroll(props) {
+	const { children, window } = props;
+	// Note that you normally won't need to set the window ref as useScrollTrigger
+	// will default to window.
+	// This is only being set here because the demo is in an iframe.
+	const trigger = useScrollTrigger({
+	  disableHysteresis: true,
+	  threshold: 0,
+	  target: window ? window() : undefined,
+	});
+  
+	return React.cloneElement(children, {
+	  elevation: trigger ? 4 : 0,
+	});
+  }
+  
+  ElevationScroll.propTypes = {
+	children: PropTypes.element.isRequired,
+	/**
+	 * Injected by the documentation to work in an iframe.
+	 * You won't need it on your project.
+	 */
+	window: PropTypes.func,
+  };
+
+function Header(props) {
 	const classes = useStyles();
 	return (
 		<React.Fragment>
 			<CssBaseline />
-			<AppBar
-				position="static"
-				color="default"
-				elevation={0}
-				className={classes.appBar}
-			>
-				<Toolbar className={classes.toolbar}>
-					<Typography
-						variant="h6"
-						color="inherit"
-						noWrap
-						className={classes.toolbarTitle}
-					>
-						<Link
-							component={NavLink}
-							to="/"
-							underline="none"
-							color="textPrimary"
+			<ElevationScroll {...props}>
+				<AppBar
+					color="default"
+					elevation={0}
+					className={classes.appBar}
+				>
+					<Toolbar className={classes.toolbar}>
+						<Typography
+							variant="h6"
+							color="inherit"
+							noWrap
+							className={classes.toolbarTitle}
 						>
-							Malcolm Sage
-						</Link>
-					</Typography>
-                    < SimpleMenu />
-				</Toolbar>
-			</AppBar>
+							<Link
+								component={NavLink}
+								to="/"
+								underline="none"
+								color="textPrimary"
+							>
+								Malcolm Sage
+							</Link>
+						</Typography>
+						< SimpleMenu />
+					</Toolbar>
+				</AppBar>
+			</ElevationScroll>
+			<br/>
 		</React.Fragment>
 	);
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 
@@ -7,23 +6,31 @@ import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 
 import App from './App';
 import Header from './components/header';
-// import Footer from './components/footer';
+import Footer from './components/footer';
 import About from './components/about';
 import Projects from './components/projects';
 import Contact from './components/contact';
 import reactDom from 'react-dom';
 
+import Toolbar from '@material-ui/core/Toolbar';
+import { Container } from '@material-ui/core';
+// import CssBaseline from '@material-ui/core/CssBaseline';
+
 const routing = (
   <Router>
     <React.StrictMode>
-      <Header />
-      <Switch>
-				<Route exact path="/" component={App} />
-				<Route exact path="/about" component={About} />
-				<Route exact path="/projects" component={Projects} />
-				<Route exact path="/contact" component={Contact} />
-      </Switch>
-      {/* <Footer /> */}
+      <Container maxWidth='md'>
+        {/* <CssBaseline /> */}
+        <Toolbar />
+        <Header />
+        <Switch>
+          <Route exact path="/" component={App} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/projects" component={Projects} />
+          <Route exact path="/contact" component={Contact} />
+        </Switch>
+      </Container>
+      <Footer />
     </React.StrictMode>,
   </Router>
 );
