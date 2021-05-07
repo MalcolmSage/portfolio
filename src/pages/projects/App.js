5fc2bce2b97f6f08d67cdee115a80c6f3c30d24d
ThatLivelyLlama <thatlivelyllama@gmail.com>
2021-05-07 15:17:31 -0600

    Added a featured projects section

--- src/pages/projects/App.js
+++ src/pages/projects/App.js
@@ -2,26 +2,71 @@ import React, { useEffect, useState } from 'react';
 // import { makeStyles } from '@material-ui/core/styles';
 import Projects from './components/projects';
 import ProjectLoadingComponent from './components/projectLoading';
+import Featured from './components/featured';
+import FeaturedLoadingComponent from './components/featuredLoading';
 import axiosInstance from '../../axios';
 
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        marginBottom: 10,
+    },
+}));
+
+
 function Project() {
+    const classes = useStyles();
     const ProjectLoading = ProjectLoadingComponent(Projects);
+    const FeaturedLoading = FeaturedLoadingComponent(Featured);
     const [projectState, setProjectState] = useState({
         loading: true,
+        fLoading: true,
         projects: null,
+        featured: null,
     });
-
     useEffect(() => {
         axiosInstance.get('projects/').then((res) => {
             const allProjects = res.data;
-            setProjectState({ loading: false, projects: allProjects });
+            const allFeatured = allProjects.filter(project => project.featured === true)
+            setProjectState({ loading: false, projects: allProjects, featured: allFeatured });
+            // console.log(allFeatured)
+            // console.log(allProjects)
         });
     }, [setProjectState]);
 
     return (
         <div className='Project'>
-            <h1>Projects</h1>
+        <Grid container>   
+            <Grid container justify='center' className={classes.root}>
+                <Box bgcolor="#4E5B31" p={.5} marginTop='11px' borderRadius="borderRadius">
+                    <Typography variant="h3" align='center'>
+                        <Box color="white">
+                            Featured Projects
+                        </Box>
+                    </Typography>
+                </Box>
+            </Grid>
+            <FeaturedLoading isLoading={projectState.loading} projects={projectState.featured} />
+            <Grid container justify='center' className={classes.root} >
+                <Box bgcolor="#4E5B31" p={.5} marginTop='11px' borderRadius="borderRadius" >
+                    <Typography variant="h4" align='center' >
+                        <Box color="white">
+                            All Projects
+                        </Box>
+                    </Typography>
+                </Box>
+            </Grid>
+
+
             <ProjectLoading isLoading={projectState.loading} projects={projectState.projects} />
+        </Grid> 
         </div>
     );
 }
--- src/pages/projects/components/featured.js
+++ src/pages/projects/components/featured.js
@@ -35,7 +35,7 @@ const useStyles = makeStyles((theme) => ({
 	},
 }));
 
-const Projects = (props) => {
+const Featured = (props) => {
 	const { projects } = props;
 	const classes = useStyles();
 	if (!projects || projects.length === 0) return <p>Can not find any projects, sorry</p>;
@@ -44,47 +44,49 @@ const Projects = (props) => {
 			<Container maxWidth="md" component="main">
 				<Grid container spacing={2} alignItems="flex-end">
 					{projects.map((project) => {
-						return (
-							<Grid item key={project.id} xs={12} md={4}>
-								<Card className={classes.card}>
-									<Link
-										color="textPrimary"
-										href={'project/' + project.slug}
-										className={classes.link}
-									>
-										<CardMedia
-											className={classes.cardMedia}
-											image="https://source.unsplash.com/random"
-											title="Image title"
-										/>
-									</Link>
-									<CardContent className={classes.cardContent}>
-										<Typography
-											gutterBottom
-											variant="h6"
-											component="h2"
-											className={classes.projectTitle}
+						if (project.featured) {
+							return (
+								<Grid item key={project.id} xs={12} md={4}>
+									<Card className={classes.card}>
+										<Link
+											color="textPrimary"
+											href={'project/' + project.slug}
+											className={classes.link}
 										>
-											{project.title.substr(0, 25)}
-										</Typography>
-										<div className={classes.projectText}>
-											<Typography color="textSecondary">
-												{project.excerpt.substr(0, 40)}...
+											<CardMedia
+												className={classes.cardMedia}
+												image="https://source.unsplash.com/random"
+												title="Image title"
+											/>
+										</Link>
+										<CardContent className={classes.cardContent}>
+											<Typography
+												gutterBottom
+												variant="h6"
+												component="h2"
+												className={classes.projectTitle}
+											>
+												{project.title.substr(0, 25)}
 											</Typography>
-										</div>
-										<div className={classes.projectText}>
-										<Typography color="textSecondary">
-												{project.tags.map((tag) => tag + ' ')}
-										</Typography>
-										</div>
-									</CardContent>
-								</Card>
-							</Grid>
-						);
+											<div className={classes.projectText}>
+												<Typography color="textSecondary">
+													{project.excerpt.substr(0, 40)}...
+												</Typography>
+											</div>
+											<div className={classes.projectText}>
+											<Typography color="textSecondary">
+													{project.tags.map((tag) => tag + ' ')}
+											</Typography>
+											</div>
+										</CardContent>
+									</Card>
+								</Grid>
+							);
+						}	
 					})}
 				</Grid>
 			</Container>
 		</React.Fragment>
 	);
 };
-export default Projects;
+export default Featured;
